238e4980bb9033cdac3e58d9e7699d1f2de00fbd
vinsky001 <ekibet544@gmai.com>
2023-07-21 03:58:58 -0700

    Flask application that displays 'python and replaces  'text with 'is cool'

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application:
+listens on 0.0.0.0, port 5000
+Routes:	/: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C” followed by the value of text
+    displays “Python ”, followed by the value of the text
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """function for web app route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """displays /hbnb web app route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """dispalys the value of /c/<text>"""
+    text = text.replace("_", " ")
+    return "C %s" % text
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """displays the value of /python/<text>"""
+    text = text.replace("_", " ")
+    return "Python %s" % text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
