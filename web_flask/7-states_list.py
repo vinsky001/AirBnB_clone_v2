1e7908fef2fdea9ed70143ebbda9d1652044cfbb
vinsky001 <ekibet544@gmail.com>
2023-07-23 01:06:02 -0700

    fetching data from the storage engine

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+stats flask application
+must be listening on 0.0.0.0, port 5000
+"""
+import os
+from flask import Flask, render_template
+from models.state import State
+from models import Storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays list of all State objects in DBStorage.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
