a7fbd0717d18c045ed584ce80967d2a0d0db0141
root <ekibet544@gmail.com>
2023-08-16 15:09:24 -0700

    Fixed the app debug mode to none

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_route():
     """Returns a string route"""
-    return render_template("0-index.html")
+    return render_template("9-states.html")
 
 
 if __name__ == "__main__":
