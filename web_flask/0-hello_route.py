d14cfc508dea8063d68fe7de4fc7183163c53d2a
vinsky001 <ekibet544@gmail.com>
2023-08-31 08:12:13 +0000

    Fix app to run on port 5000

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ flask script """
 
-from flask import Flask, render_template
+from flask import Flask
 
 
 app = Flask(__name__)
@@ -10,8 +10,8 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_route():
     """Returns a string route"""
-    return render_template("9-states.html")
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000)
